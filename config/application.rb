a486778343a001f777dd47a8cf106fa852ffd1b0
mizhal <mizhal@gmail.com>
2016-06-23 18:16:10 +0000

    Remove ActiveRecord

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "rails/test_unit/railtie"
+require "action_mailer/railtie"
+require "active_model"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,9 +22,6 @@ module LordOfTheTweets
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += ["#{config.root}/lib"]
   end
